c146571ca062356188682b865562ae9aed8457df
Mark Delk <jethrodaniel@gmail.com>
2021-03-27 21:22:13 -0500

    add rbenv and yarn to install script

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ log() {
 
 log 'installing prereqs...'
 sudo apt update -y
-sudo apt install -y make git vim tmux postgresql tree xclip rsync i3 i3lock redshift firefox xterm
+sudo apt install -y make git vim tmux postgresql tree xclip rsync i3 i3lock redshift firefox xterm npm
 
 if [ -d ~/dotfiles ]; then
   log 'found dotfiles'
@@ -12,3 +12,17 @@ else
   log 'no dotfiles, cloning'
   git clone --recursive https://github.com/jethrodaniel/dotfiles
 fi
+
+if [ -d ~/.rbenv ]; then
+  log 'found rbenv'
+else
+  log 'no rbenv, cloning'
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  mkdir -p "$(rbenv root)"/plugins
+  git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+fi
+
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+
+log 'yarn/js'
+sudo npm install -g yarn
